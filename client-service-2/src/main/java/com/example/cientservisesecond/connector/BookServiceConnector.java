2df77617f5c54e676bdc4aed8ba20db5ecc0eb7c
Igor Anikeev <anikeev@softlab.ru>
2023-12-22 18:42:17 +0400

    7.1.3

--- client-service-2/src/main/java/com/example/cientservisesecond/connector/BookServiceConnector.java
+++ client-service-2/src/main/java/com/example/cientservisesecond/connector/BookServiceConnector.java
@@ -1,14 +1,18 @@
 package com.example.cientservisesecond.connector;
 
 import com.example.cientservisesecond.model.Book;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = "BOOK-SERVICE")
+@FeignClient(name = "BOOK-SERVICE", fallbackFactory = BookServiceConnector.BookServiceConnectorFallbackFactory.class)
 public interface BookServiceConnector {
     @GetMapping("/api/books")
     List<Book> getAllBooks();
@@ -18,5 +22,32 @@ public interface BookServiceConnector {
 
     @DeleteMapping("/api/books")
     String deleteBooks();
+
+    @Component
+    class BookServiceConnectorFallbackFactory implements FallbackFactory<FallbackWithFactory> {
+
+        @Override
+        public FallbackWithFactory create(Throwable cause) {
+            return new FallbackWithFactory(cause.getMessage());
+        }
+    }
+
+    @Slf4j
+    record FallbackWithFactory(String reason) implements BookServiceConnector {
+        @Override
+        public List<Book> getAllBooks() {
+            return new ArrayList<Book>();
+        }
+
+        @Override
+        public String createBooks() {
+            return "Отработал Fallback";
+        }
+
+        @Override
+        public String deleteBooks() {
+            return "Отработал Fallback";
+        }
+    }
 }
 
--- client-updater/src/main/java/com/example/cientupdater/connector/ClientServiceConnector.java
+++ client-updater/src/main/java/com/example/cientupdater/connector/ClientServiceConnector.java
@@ -1,12 +1,38 @@
 package com.example.cientupdater.connector;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "CLIENT-SERVICE")
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@FeignClient(name = "CLIENT-SERVICE", fallbackFactory = ClientServiceConnector.ClientServiceConnectorFallbackFactory.class)
 public interface ClientServiceConnector {
 
     @GetMapping("/api/client/test")
     String test();
-}
 
+    @Component
+    class ClientServiceConnectorFallbackFactory implements FallbackFactory<FallbackWithFactory> {
+
+        @Override
+        public FallbackWithFactory create(Throwable cause) {
+            return new FallbackWithFactory(cause.getMessage());
+        }
+    }
+
+    @Slf4j
+    record FallbackWithFactory(String reason) implements ClientServiceConnector {
+
+        @Override
+        public String test() {
+            return "Отработал Fallback";
+        }
+    }
+}
